6c72519fb4ed41667aa23969d276b1e8bf2bfccc
scammi <scammi@gmail.com>
2022-10-25 14:35:54 -0300

    Yarn scripts

--- scripts/foundTestAccount.js
+++ scripts/foundTestAccount.js
@@ -2,13 +2,13 @@
 const { ethers } = require("hardhat");
 
 // RUN 
-// npx hardhat run --network hardhat scripts/foundTestAccount.js
+// npx hardhat run --network localhost scripts/foundTestAccount.js
 
 (async () => {
   const soul = await ethers.getContract("Soul")
   const mintPromises = [1, 2, 3, 4].map(() => {
     return soul.safeMint(
-      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', // to
       'tokenUri.com',
     );
   });
